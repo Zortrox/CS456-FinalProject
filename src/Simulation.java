8257c152bbda7fce743b65a05b6c370537382849
Jared <jareda.prince@ymail.com>
2016-11-14 16:21:31 -0600

    project 2 in for CS 421

--- src/Simulation.java
+++ src/Simulation.java
@@ -11,5 +11,51 @@ public class Simulation {
 		//	}
 		//}
 
+		public JFrame frame;
+		public JPanel controlPanel = new JPanel();
+		boolean running = true;
+		
+		public static void main(String args[]){
+			new Simulation();
+		}
+		
+		public Simulation(){
+			//sets up the frame
+			frame = new JFrame();
+			frame.setTitle("Ant Colony Simulation");
+			frame.setVisible(true);
+			frame.setResizable(false);
+			frame.setSize(900, 700);
+			frame.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					System.exit(0);
+				}
+			});
+			
+			JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+			pane.setDividerLocation(700);
+			pane.add(this);
+			pane.add(controlPanel);
+			
+			frame.add(pane);
+			
+			Colony colony = new Colony();
+			
+			
+			while(running){
+
+			}
+		}
+		
+		public void paint(Graphics g){
+			g.fillRect(0, 0, 700, 700);
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }
